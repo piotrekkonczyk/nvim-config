4970f8818ca33990a7fecea286418b555105c134
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-06-22 09:42:59 +0200

    python if __name__ snippet

--- lua/snippets/python.lua
+++ lua/snippets/python.lua
@@ -1,12 +1,17 @@
-local ls = require 'luasnip'
+local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
 return {
-  s('sol', {
-    t { 'solution = Solution()', 'print(solution.' },
-    i(1),
-    t ')',
-  }),
+	s("sol", {
+		t({ "solution = Solution()", "print(solution." }),
+		i(1),
+		t(")"),
+	}),
+
+	s("ifn", {
+		t({ 'if __name__ == "__main__":', "\t" }),
+		i(0),
+	}),
 }
