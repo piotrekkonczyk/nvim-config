fb6e1a9f9192e35ab6c65d203863bc3766d7f56c
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-06-22 22:48:07 +0200

    theme filters the black tildes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,14 +1,26 @@
-return { -- You can easily change to a different colorscheme.
-  'folke/tokyonight.nvim',
-  priority = 1000,
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
-      styles = {
-        comments = { italic = false }, -- Disable italics in comments
-      },
-    }
+return {
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("tokyonight").setup({
+			transparent = true,
+			styles = {
+				comments = { italic = false }, -- Disable italics in comments
+			},
+		})
 
-    vim.cmd.colorscheme 'tokyonight-night'
-  end,
+		-- Set the colorscheme
+		vim.cmd.colorscheme("tokyonight-night")
+
+		-- Hide tildes (~) at the end of buffer
+		vim.opt.fillchars:append({ eob = " " })
+
+		-- Optional: make background transparent even if theme resets it
+		vim.cmd([[
+      hi Normal guibg=NONE
+      hi NormalNC guibg=NONE
+      hi EndOfBuffer guibg=NONE
+    ]])
+	end,
 }
